e047d51b37ed21363fabd519d0f7eb98f7a69312
Anthony Brogni <anthonybrog@gmail.com>
2019-11-29 14:23:33 -0600

    Attempt to fix problems

--- src/main/java/org/usfirst/frc/team3042/robot/commands/Spinner_AdjustSpeed.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/Spinner_AdjustSpeed.java
@@ -47,7 +47,7 @@ public class Spinner_AdjustSpeed extends Command {
 	 * Make this return true when this Command no longer needs to run execute()
 	 */
 	protected boolean isFinished() {
-		return !spinner.closedLoop.spinning;
+		return false;
 	}
 
 	/** end *******************************************************************
@@ -69,5 +69,6 @@ public class Spinner_AdjustSpeed extends Command {
 	
 	/** Graceful End **********************************************************/
 	private void terminate() {
+		spinner.stop();
 	}
 }
